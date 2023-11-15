2d704288079b8199f169d0ed55d72a11dac4a7bf
Awwal <sakeebmawwal@gmail.com>
2023-11-15 03:31:04 +0000

    IT WORKS after code revamp with tutor

--- starter/script.js
+++ starter/script.js
@@ -1,62 +1,65 @@
 // Variable Declaration
 var timeBlock = $(".time-block");
 var currentDay = $("#currentDay");
-//var currentHour = $(".hour")
+var currentHour = $(".hour")
 
-var currentHour = document.querySelectorAll(".hour")
 
-var eachCurrentHour 
+// if ($(timeBlock).attr("id") == 10){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+// console.log($(timeBlock.attr("id")))
+
+
+
+
+
 
 
 
 // set current Day
 var today = dayjs();
 var currentToday = today.format('dddd, MMMM D[th]');
-currentDay.text(currentToday);
+
+var presentHour = today.hour();
 
 
-var presentHour = today.format("h");
-var presentHourInt = parseInt(presentHour)
-console.log(presentHourInt);
+// console.log(timeBlock[0])
 
-
-//$(".time-block").css("background-color","red")
-//console.log(timeBlock);
-
-for (var i=0; i<timeBlock.length; i++) { 
-    var eachTimeBlock = timeBlock[i] 
-    for(var k=0; k<currentHour.length; k++){
-        var currentHourValue = parseInt(currentHour[k].textContent)
-        if (presentHourInt === currentHourValue){
-            console.log(currentHour[k].textContent)
-            $(eachTimeBlock).css("background-color","red")
-            break;
-        }
-        
-    }  
+for (let index = 0; index < timeBlock.length; index++) {
+    var elements = $(timeBlock[index]).attr("id");
+    var elementsInt = parseInt(elements)
+    //console.log(elements)
+    //console.log(typeof(elementsInt))
+    if (elementsInt == presentHour){
+       $(timeBlock[index]).addClass("present")
+    }
+    
 };
 
-//for(var k=0; k<currentHour.length; k++)
-
-
-// for (let index = 0; index < timeBlock.length; index++) {
-//     const eachTimeBlock = timeBlock[index]
-//     if (presentHour == 4){
-//         $(eachTimeBlock).css("background-color","red")
-//     }
-    
-// }
-
-
-// for(var k=0; k<currentHour.length; k++){
-//     if (currentHour[k].textContent == presentHourInt){
-//         console.log(currentHour[k].textContent)
-//     }
-    
-    //console.log( currentHour[k].textContent)
-//}   
 
 
 
 
+// 1. TODO: add the current date to page when it loads.
+currentDay.text(currentToday);
+// console.log(typeof(presentHour))
+// 2. TODO: using logic, figure out how to add the classes of past, present or future to the time block depending on the time of the day
 
+// 3. TODO: when the saveBtn is clicked we need to save the content of the textarea into localStorage. 
+$('.saveBtn').on('click', function(e){
+    // TODO: capture the user input from the textarea
+var key = $(e.target).parent().attr('id')
+var value = $(e.target).siblings('textarea').val()
+
+
+
+console.log(key);
+    // store into localStorage
+     localStorage.setItem(key, value)
+})
+
+// 4. TODO: make sure the data that is stored in localStorage presists on the page. 
